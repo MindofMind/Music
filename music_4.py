cf31d7e2ad8e7de389332cc1ed922d5ffc345f82
MindofMind <67787706+MindofMind@users.noreply.github.com>
2020-07-24 11:00:08 +0100

    Update music_4.py

--- music_4.py
+++ music_4.py
@@ -15,3 +15,10 @@ plt.figure(figsize=(5,5))
 
 #plot
 plt.hist(no)
+
+#From the above plot, we can infer that most of the notes have a very low frequency. 
+#So, let us keep the top frequent notes and ignore the low-frequency ones. 
+#Here, I am defining the threshold as 50. Nevertheless, the parameter can be changed.
+
+frequent_notes = [note_ for note_, count in freq.items() if count>=50]
+print(len(frequent_notes))
