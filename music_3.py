f84631b4184f219840fcef16fd6ea6f5b631341a
MindofMind <67787706+MindofMind@users.noreply.github.com>
2020-07-24 10:56:40 +0100

    Update music_3.py

--- music_3.py
+++ music_3.py
@@ -1,4 +1,7 @@
+#Understanding the data:
+#Under this section, we will explore the dataset and understand it in detail.
 #converting 2D array into 1D array
+
 notes_ = [element for note_ in notes_array for element in note_]
 
 #No. of unique notes
