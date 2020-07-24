ba6c532c7d5c45cc04be358a2c3cc034db55c0cf
MindofMind <67787706+MindofMind@users.noreply.github.com>
2020-07-24 11:08:45 +0100

    Update music_7.py

--- music_7.py
+++ music_7.py
@@ -1,3 +1,9 @@
+#Now, we will assign a unique integer to every note:
+
+unique_x = list(set(x.ravel()))
+x_note_to_int = dict((note_, number) for number, note_ in enumerate(unique_x))
+
+
 #preparing input sequences
 x_seq=[]
 for i in x:
