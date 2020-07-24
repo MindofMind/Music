2c747fd079b630bbcff003a9b255bd5c94c0c347
MindofMind <67787706+MindofMind@users.noreply.github.com>
2020-07-24 11:21:13 +0100

    Add files via upload

---
+++ 10_9.py
@@ -0,0 +1,18 @@
+import random
+ind = np.random.randint(0,len(x_val)-1)
+
+random_music = x_val[ind]
+
+predictions=[]
+for i in range(10):
+
+    random_music = random_music.reshape(1,no_of_timesteps)
+
+    prob  = model.predict(random_music)[0]
+    y_pred= np.argmax(prob,axis=0)
+    predictions.append(y_pred)
+
+    random_music = np.insert(random_music[0],len(random_music[0]),y_pred)
+    random_music = random_music[1:]
+    
+print(predictions)
