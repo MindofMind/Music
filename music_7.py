ee4e3c9f374acf508ad9466f6b26ac1dcc39950b
MindofMind <67787706+MindofMind@users.noreply.github.com>
2020-07-24 11:08:11 +0100

    Add files via upload

---
+++ music_7.py
@@ -0,0 +1,10 @@
+#preparing input sequences
+x_seq=[]
+for i in x:
+    temp=[]
+    for j in i:
+        #assigning unique integer to every note
+        temp.append(x_note_to_int[j])
+    x_seq.append(temp)
+    
+x_seq = np.array(x_seq)
