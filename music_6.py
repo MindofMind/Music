9da26080abdcef27e6a1c8c64850b52977a81819
MindofMind <67787706+MindofMind@users.noreply.github.com>
2020-07-24 11:06:04 +0100

    Add files via upload

---
+++ music_6.py
@@ -0,0 +1,16 @@
+no_of_timesteps = 32
+x = []
+y = []
+
+for note_ in new_music:
+    for i in range(0, len(note_) - no_of_timesteps, 1):
+        
+        #preparing input and output sequences
+        input_ = note_[i:i + no_of_timesteps]
+        output = note_[i + no_of_timesteps]
+        
+        x.append(input_)
+        y.append(output)
+        
+x=np.array(x)
+y=np.array(y)
