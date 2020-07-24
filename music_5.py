559c104e2960730c390116e8cd1e49eec82ce5c3
MindofMind <67787706+MindofMind@users.noreply.github.com>
2020-07-24 11:02:33 +0100

    Add files via upload

---
+++ music_5.py
@@ -0,0 +1,10 @@
+new_music=[]
+
+for notes in notes_array:
+    temp=[]
+    for note_ in notes:
+        if note_ in frequent_notes:
+            temp.append(note_)            
+    new_music.append(temp)
+    
+new_music = np.array(new_music)
